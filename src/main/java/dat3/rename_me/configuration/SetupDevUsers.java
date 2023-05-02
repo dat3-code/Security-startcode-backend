09dca586cd14509aec30605a954eb59a18b7dead
Nicolai Andersen <niiicolai@gmail.com>
2023-05-02 14:11:40 +0200

    Use environment profile to avoid executing development code in production

--- src/main/java/dat3/rename_me/configuration/SetupDevUsers.java
+++ src/main/java/dat3/rename_me/configuration/SetupDevUsers.java
@@ -4,11 +4,14 @@ import dat3.security.entity.Role;
 import dat3.security.entity.UserWithRoles;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import dat3.security.repository.UserWithRolesRepository;
 
 import static dat3.security.config.UsersForDevelopmentOnly.setupUserWithRoleUsers;
+
+import java.util.Arrays;
 
 @Controller
 public class SetupDevUsers implements ApplicationRunner {
@@ -16,14 +19,20 @@ public class SetupDevUsers implements ApplicationRunner {
     UserWithRolesRepository userWithRolesRepository;
     PasswordEncoder passwordEncoder;
 
-    public SetupDevUsers(UserWithRolesRepository userWithRolesRepository, PasswordEncoder passwordEncoder) {
+    Environment env;
+
+    public SetupDevUsers(UserWithRolesRepository userWithRolesRepository, PasswordEncoder passwordEncoder, Environment env) {
         this.userWithRolesRepository = userWithRolesRepository;
         this.passwordEncoder = passwordEncoder;
+        this.env = env;
     }
 
-    //Remove me before production
     @Override
     public void run(ApplicationArguments args) {
+        // Prevents this code from running on Azure, where the profile is set to "prod"
+        if (!Arrays.asList(env.getActiveProfiles()).contains("dev"))
+            return;
+
         setupUserWithRoleUsers(userWithRolesRepository, passwordEncoder);
     }
 }
