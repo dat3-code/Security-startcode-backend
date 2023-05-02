1a7199be6adcc1a0947bd3df5e5bc8ba81f0c47a
Nicolai Andersen <niiicolai@gmail.com>
2023-05-02 14:20:49 +0200

    Use @Profile annotation instead

--- src/main/java/dat3/rename_me/configuration/SetupDevUsers.java
+++ src/main/java/dat3/rename_me/configuration/SetupDevUsers.java
@@ -4,6 +4,7 @@ import dat3.security.entity.Role;
 import dat3.security.entity.UserWithRoles;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -11,9 +12,8 @@ import dat3.security.repository.UserWithRolesRepository;
 
 import static dat3.security.config.UsersForDevelopmentOnly.setupUserWithRoleUsers;
 
-import java.util.Arrays;
-
 @Controller
+@Profile("dev") // Prevents this code from running on Azure, where the profile is set to "prod"
 public class SetupDevUsers implements ApplicationRunner {
 
     UserWithRolesRepository userWithRolesRepository;
@@ -29,10 +29,6 @@ public class SetupDevUsers implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        // Prevents this code from running on Azure, where the profile is set to "prod"
-        if (!Arrays.asList(env.getActiveProfiles()).contains("dev"))
-            return;
-
         setupUserWithRoleUsers(userWithRolesRepository, passwordEncoder);
     }
 }
