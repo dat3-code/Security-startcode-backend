31beae2640cab37d4cc35307cce6ef22bf9bf7a4
Lars Åke Mortensen <LMOR@kea.dk>
2023-05-04 08:34:15 +0200

    minor

--- src/main/java/dat3/security/config/SecurityConfig.java
+++ src/main/java/dat3/security/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -37,7 +38,7 @@ import javax.crypto.spec.SecretKeySpec;
 import java.util.Collections;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)//,jsr250Enabled = true)
+@EnableMethodSecurity(prePostEnabled = true)
 
 @Configuration
 public class SecurityConfig {
